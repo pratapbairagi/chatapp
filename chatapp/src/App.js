3a4d743b6d97aa3f85b94f343dfa74072a3af2ba
PratapBairagi2 <pratap.bairagi.test@gmail.com>
2022-08-06 20:09:06 +0530

    frontend socket comnfig change

--- chatapp/src/App.js
+++ chatapp/src/App.js
@@ -6,8 +6,14 @@ import Main from "./components/chat/main";
 import "./App.css"
 
 // const socket = io("http://localhost:4999")
-const ENDPOINT = "http://localhost:4999"
-const socket= io(ENDPOINT, {transports:['websocket']})
+// const ENDPOINT = "http://localhost:4999"
+// const socket= io(ENDPOINT, {transports:['websocket']})
+// const socket= io("ws://localhost:4999")
+var socket = io.connect("http://localhost:4999", {
+   forceNew: true,
+   transports: ["polling"],
+});
+
 
 
 function App() {
